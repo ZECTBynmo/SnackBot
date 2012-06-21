a7eb930a82108649491b0c7f2d0d5f333421b1e0
Mike Vegeto <ZECTBynmo@.(none)>
2012-06-20 23:33:06 -0400

    allow index.html to request the list of snackers and display it as <li> objects

--- snackbot.js
+++ snackbot.js
@@ -1,21 +1,42 @@
 HOST = null; // localhost
 PORT = 5325;
 
+//----------------------------------------------------------------------------------------------------
+//	Requires
+//----------------------------------------------------------------------------------------------------		
 var oscar = require("oscar");
 var ServerTools = require("./ServerTools");
 var AIMTools = require("./AIMTools");
 var authInfo = require("./AuthInfo");
 
-// Our map of people who want snack time notifications
-var snackers = new Array();
+//----------------------------------------------------------------------------------------------------
+//	File scope variables
+//----------------------------------------------------------------------------------------------------		
+var snackers = new Array();						// Our array of people who want snack time notifications
+var port = Number(process.env.PORT || PORT);	// Start the server on the process specified port if it 
+												// exists, otherwise fall back on PORT
 
-// Start the server on the process specified port if it exists, fall back on PORT
-var port = Number(process.env.PORT || PORT);
+
+//----------------------------------------------------------------------------------------------------
+//	Server
+//----------------------------------------------------------------------------------------------------											
 ServerTools.listen(port, HOST);
 
 // Create a file handler for index.html, so clients can receive it
 ServerTools.addRequestHandler("/", ServerTools.createFileHandler("index.html"));
+ServerTools.addRequestHandler("/jquery-1.7.1.min.js", ServerTools.createFileHandler("jquery-1.7.1.min.js"));
 
+// Respond to the client requesting the current list of snackers
+ServerTools.addRequestHandler("/getSnackers", function(request, response) {
+	response.respondJSON(200, {
+		snackers: snackers
+	});
+});
+
+
+//----------------------------------------------------------------------------------------------------
+//	AIM
+//----------------------------------------------------------------------------------------------------	
 // Connect to aim
 AIMTools.connect( authInfo.screenName, authInfo.password );
 
@@ -26,10 +47,9 @@ AIMTools.addKeyResponse( "add me", "Okay, adding you", function(sender, text) {
 	addSnacker( sender );
 });
 
-AIMTools.addKeyResponse( "trigger snacks", "THE TIME IS UPON US", function( sender, text ) {
+AIMTools.addKeyResponse( "trigger snacks", "THE TIME IS UPON US", function(sender, text) {	
 	triggerSnacks( sender );
 });
-
 
 // Adds a snacker to the list of people who want to be notified of snacks
 function addSnacker( screenName ) {
@@ -37,11 +57,14 @@ function addSnacker( screenName ) {
 
 	// Look through our list of snackers and make sure we don't have this screen name already
 	for( iSnacker=0; iSnacker<snackers.length; ++iSnacker ) {
-		if( snackers[iSnacker] == screenName )
+		if( snackers[iSnacker].name == screenName ) {
 			isSnackerInList = true;
+			break;
+		}
 	} // end for each snacker
 	
-	if( !isSnackerInList ) {	
+	if( !isSnackerInList ) {
+		var newSnacker = { name: screenName }
 		snackers.push( screenName );
 		console.log( screenName + " wants snacks. We have " + snackers.length + " snackers now" );
 	} else {
@@ -50,10 +73,9 @@ function addSnacker( screenName ) {
 	}
 } // end addSnacker()
 
-
 // Notify the snackers that their time has come
 function triggerSnacks( triggeredBy ) {
 	for( var iSnacker=0; iSnacker<snackers.length; ++iSnacker ) {
-		AIMTools.sendMessage( snackers[iSnacker], "SNACKS! Triggered by " + triggeredBy );
+		AIMTools.sendMessage( snackers[iSnacker].name, "SNACKS! Triggered by " + triggeredBy );
 	}
 } // end triggerSnacks()
